5199c54af29d4a05f8901aa67989cbda0c956cc7
Nandini <nandini.bandreddi@conscendo.io>
2025-01-17 08:59:04 +0530

    job list cards updated

--- components/successStoryDetails/index.jsx
+++ components/successStoryDetails/index.jsx
@@ -70,19 +70,3 @@ const SuccessStoryDetails = ({ blok }) => {
 
 export default SuccessStoryDetails;
 
-export async function getStaticProps() {
-  let sbParams = {
-    version: process.env.STORYBLOK_VERSION, // or 'published'
-    token: process.env.STORYBLOK_ACCESS_TOKEN,
-  };
-
-  const storyblokApi = getStoryblokApi();
-  let { data: config } = await storyblokApi.get("cdn/stories/config", sbParams);
-
-  return {
-    props: {
-      config: config ? config.story : false,
-    },
-    revalidate: 3600,
-  };
-}
