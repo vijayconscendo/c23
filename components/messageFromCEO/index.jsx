6f0cb8742e67d4898d41d9e2dd43ba58ee453a93
vijayconscendo <vijayconscendo@gmail.com>
2025-01-22 18:44:00 +0530

    news blocks added

--- components/messageFromCEO/index.jsx
+++ components/messageFromCEO/index.jsx
@@ -2,20 +2,28 @@ import { StoryblokComponent, storyblokEditable } from "@storyblok/react";
 import React from "react";
 import { render } from "storyblok-rich-text-react-renderer";
 import ImageWrapper from "../storyblokComponents/imageWrapper";
+import styles from "./messafeFromCEO.module.scss";
 
 function MessageFromCEO({ blok }) {
   return (
-    <section {...storyblokEditable(blok)}>
+    <section className={styles.messageFromCeo} {...storyblokEditable(blok)}>
       {blok?.title?.[0] && <StoryblokComponent blok={blok?.title?.[0]} />}
-      <div className="flex">
-        <div>
+      <div
+        className={`w-full flex flex-col md:flex-row gap-y-10 gap-x-10 lg:gap-x-[92px] md:gap-x-[82px] ${styles.gridblock}`}
+      >
+        <div
+          className={`order-2 md:order-1 w-full md:w-8/12 text-black ${styles.text}`}
+        >
           {blok?.message?.length > 0 &&
             blok.message.map((msg) => render(msg?.text))}
         </div>
-        {blok?.image?.[0] && (
-          <ImageWrapper blok={blok.image[0]} altText="CEO" />
-        )}
-        <div></div>
+        <div className="order-1 md:order-2 w-full md:w-4/12">
+          <div className="w-full max-w-[529px">
+            {blok?.image?.[0] && (
+              <ImageWrapper blok={blok.image[0]} altText="CEO" />
+            )}
+          </div>
+        </div>
       </div>
     </section>
   );
--- components/mission-vision/index.jsx
+++ components/mission-vision/index.jsx
@@ -1,19 +1,22 @@
 import { StoryblokComponent, storyblokEditable } from "@storyblok/react";
 import React from "react";
 import ImageWrapper from "../storyblokComponents/imageWrapper";
+import styles from "./mission-vision.module.scss";
 
 function MissionAndVision({ blok }) {
   return (
-    <section {...storyblokEditable(blok)}>
+    <section className={styles.missionAndVision} {...storyblokEditable(blok)}>
       {blok?.title?.[0] && <StoryblokComponent blok={blok.title[0]} />}
-      <div className="flex">
+      <div
+        className={`${styles.gridblock} flex flex-col md:flex-row md:gap-x-4 gap-y-10 w-full justify-between`}
+      >
         {blok?.content?.length > 0 &&
           blok.content.map((item) => (
-            <div key={item?._uid} className="flex">
+            <div key={item?._uid} className="flex items-start">
               <ImageWrapper blok={item?.blocks?.[0]} altText={item?.title} />
               <div>
-                <h2>{item?.title}</h2>
-                <p>{item?.description}</p>
+                <h2 className="text-primary">{item?.title}</h2>
+                <p className="text-black">{item?.description}</p>
               </div>
             </div>
           ))}
