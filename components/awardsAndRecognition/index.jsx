5e08f170085dfb7a3a6d6ef02e2bc5773dca91a5
vijayconscendo <vijayconscendo@gmail.com>
2025-01-24 15:30:35 +0530

    changes added again in carousel

--- components/awardsAndRecognition/index.jsx
+++ components/awardsAndRecognition/index.jsx
@@ -3,17 +3,27 @@ import ImageWrapper from "../storyblokComponents/imageWrapper";
 
 function AwardSection({ blok }) {
   return (
-    <section>
-      <div className="flex justify-between gap-4">
-        {blok?.image?.[0] && (
-          <ImageWrapper blok={blok.image[0]} altText={blok?.awardName} />
-        )}
+    <section className="py-14">
+      <div className="w-full flex flex-col md:flex-row justify-between gap-x-5 xl:gap-x-10 gap-y-10 items-end h-full px-0 xl:px-36 overflow-hidden">
+        <div className="w-full lg:min-w-[360px] lg:h-[420px] xl:min-w-[345px] xl:h-[460px] flex items-end md:justify-center object-fill">
+          {blok?.image?.[0] && (
+            <ImageWrapper blok={blok.image[0]} altText={blok?.awardName} />
+          )}
+        </div>
 
-        <div>
-          <p>{blok?.category}</p>
-          <h2>{blok?.awardName}</h2>
-          <h3>{blok?.year}</h3>
-          <p>{blok?.description}</p>
+        <div className="w-full md:w-auto">
+          <p className="py-0.5 px-4 text-white text-lg lg:text-xl xl:text-2xl rounded-full bg-primary inline-flex mb-4 leading-normal">
+            {blok?.category}
+          </p>
+          <h2 className="text-2xl lg:text-3xl xl:text-4xl mb-2 leading-relaxed font-semibold text-primary">
+            {blok?.awardName}
+          </h2>
+          <h3 className="text-xl lg:text-2xl xl:text-[32px] leading-relaxed font-medium text-black mb-5">
+            {blok?.year}
+          </h3>
+          <p className="text-lg lg:text-xl xl:text-2xl leading-relaxed text-black">
+            {blok?.description}
+          </p>
         </div>
       </div>
     </section>
